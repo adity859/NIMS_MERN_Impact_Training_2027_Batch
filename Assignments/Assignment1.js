51ba5229af2d9a1239e39fe813f6032f60e4e704
imbmali <geeky.bhavesh@gmail.com>
2026-05-21 12:34:41 +0530

    Add assignment

---
+++ Assignments/Assignment1.js
@@ -0,0 +1,4 @@
+// Values and Variables
+// Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+// Log their values to the console.
