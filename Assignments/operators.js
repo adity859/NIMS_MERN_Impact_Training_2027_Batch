20d5a7fa8030c751a75fdfd605e211c113738a8c
imbmali <geeky.bhavesh@gmail.com>
2026-05-27 16:35:17 +0530

    file update

---
+++ Assignments/operators.js
@@ -0,0 +1,10 @@
+// Basic Operators
+// If your country split in half, and each half would contain half the population, then how many people would live in each half?
+
+// Increase the population of your country by 1 and log the result to the console.
+
+// Finland has a population of 6 million. Does your country have more people than Finland?
+
+// The average population of a country is 33 million people. Does you country have less people than the average country?
+
+// Based on the variables you created, create a new variable description which contains a string with this format: 'Portugal is in Europe, and its 11 million people speak portuguese'.
--- JavaScript Basics/arrays.js
+++ JavaScript Basics/arrays.js
@@ -1,10 +1,74 @@
-// Array - Array is used to store multiles value in a single variables
+// Array -> is used to store multiple values in a single variable.
 
-// Use case: Used in JS, React & Node.js
+// Why use Arrays?
+// 1. Store multiple values
+// 2. Used everywhere in JavaScript, React & Node
 
 // Creating Arrays
 
 // Method 1
-let fruits = ["Apple", "Bananna", "Orange"];
-console.log(fruits)
+// let students = ['Bhavesh', 'Riya', 'Sid'];
+// console.log(students);
+
+// Method 2
+// let numbers = [10, 20, 30, 40];
+
+// Method 3
+// let mixed = ["JavaScript", 100, true, null];
+// console.log(mixed);
+
+// Access Array Values
+
+// let students = ['Bhavesh', 'Riya', 'Sid'];
+// console.log(students[0]);
+// console.log(students[2]);
+
+// Array Length
+// let students = ['Bhavesh', 'Riya', 'Sid'];
+// console.log(students.length);
+
+
+// Modify Array Values -> Array is mutable
+// let students = ['Bhavesh', 'Riya', 'Sid'];
+// students[1] = "Nandini";
+// console.log(students);
+
+// Array Methods
+
+// 1.Push -> Add element at end.
+// let students = ['Bhavesh', 'Riya', 'Sid'];
+// students.push("Kundan");
+// console.log(students);
+
+// 2. Pop() -> Removes last element
+
+// let students = ['Bhavesh', 'Riya', 'Sid'];
+// students.pop();
+// console.log(students);
+
+// shift() - Removes first element
+// let students = ['Bhavesh', 'Riya', 'Sid'];
+// students.shift();
+// console.log(students);
+
+// unshift: Add element at beginning.
+// let students = ['Bhavesh', 'Riya', 'Sid'];
+// students.unshift("Sneha");
+// console.log(students);
+
+
+// Slice: Extract part of array & Does not modify original array
+// array.slice(start, end);
+
+// let numbers = [1, 2, 3, 4, 5];
+// let result = numbers.slice(1,4);
+// console.log(result);
+
+// Splice
+// Add, remove or replace elements.
+// Modifies original array.
+
+
+
+
 
